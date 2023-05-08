7c732d9e4099e9728ad2d5eba0be905dacd659de
Ciaran Cumiskey <ciaran.cumiskey@zinkworks.com>
2023-05-08 17:32:56 +0100

    Implemented pause menu and ability to en/disable noises.

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -7,7 +7,6 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D birdBody;
     public float flapStrength;
     public LogicScript logicScript;
-    public AudioSource chirpSound;
     private bool isAlive = true;
 
     // Start is called before the first frame update
@@ -17,9 +16,10 @@ public class BirdScript : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        if (isAlive && Input.GetKeyDown(KeyCode.Space)){
+        // Don't flap if the game is paused or if the bird is dead
+        if (!logicScript.gameIsPaused && isAlive && Input.GetKeyDown(KeyCode.Space)){
             birdBody.velocity = Vector2.up * flapStrength;
-            chirpSound.PlayOneShot(chirpSound.clip);
+            logicScript.Chirp();
         }
     }
 
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -7,23 +7,55 @@ using TMPro;
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
+    public bool gameIsPaused = false;
+
     public Text playerScoreText;
     public Text highScoreText;
+    public Text soundEnabledText;
     public GameObject gameOverScreen;
+    public GameObject pauseScreen;
     public AudioSource checkpointAudio;
     public AudioSource collisionAudio;
     public AudioSource newHighScoreAudio;
+    public AudioSource chirpAudio;
 
     private int highScore;
     private bool isHighScoreBeaten;
+    private float soundEffectsVolume;
+    private string soundEnabledTextString;
 
-    private const string PREF_HIGH_SCORE = "highScore";
+    public const string PREF_HIGH_SCORE = "highScore";
+    public const string PREF_SOUND_ENABLED = "soundEnabled";
 
     private void Start()
     {
+        // Load the high score
         highScore = PlayerPrefs.GetInt(PREF_HIGH_SCORE, 0);
         Debug.Log("Loaded high score: " + highScore.ToString());
         highScoreText.text = string.Format("High Score: {0}", highScore);
+
+        // Manage sound effects
+        soundEffectsVolume = PlayerPrefs.GetFloat(PREF_SOUND_ENABLED, 1.0f);
+        checkpointAudio.mute = soundEffectsVolume == 0.0f;
+        collisionAudio.mute = soundEffectsVolume == 0.0f;
+        newHighScoreAudio.mute = soundEffectsVolume == 0.0f;
+        chirpAudio.mute = soundEffectsVolume == 0.0f;
+    }
+
+    void Update()
+    {
+        // Don't show the pause screen if the player has already died
+        if (!gameOverScreen.activeInHierarchy && Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                ResumeGame();
+            } else
+            {
+                Debug.Log("How fast can you move in the frozen time?");
+                PauseGame();
+            }
+        }
     }
 
     [ContextMenu("Increase Score")]
@@ -34,7 +66,7 @@ public class LogicScript : MonoBehaviour
             playerScore += scoreIncrease;
             checkpointAudio.PlayOneShot(checkpointAudio.clip);
             playerScoreText.text = string.Format("Score: {0}", playerScore);
-            if(playerScore > highScore)
+            if (playerScore > highScore)
             {
                 if (!isHighScoreBeaten) // Only play the first time the user breaks their record
                 {
@@ -59,17 +91,19 @@ public class LogicScript : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        ResumeGame(); // Ensure that the game is unpaused once restarted
     }
 
     [ContextMenu("Game Over")]
     public void GameOver()
     {
+        Time.timeScale = 0.2f; // slow down the game for added dramatisation
         gameOverScreen.SetActive(true);
         collisionAudio.PlayOneShot(collisionAudio.clip);
         GameObject gameOverScoreObject = GameObject.Find("GameOverScoreText");
         TMP_Text gameOverScoreText = gameOverScoreObject.GetComponent<TMP_Text>();
         if (playerScore > highScore)
-        {            
+        {
             PlayerPrefs.SetInt(PREF_HIGH_SCORE, playerScore); //Overwrite the high score
             gameOverScoreText.text = string.Format("New High Score! {0}", playerScore);
             gameOverScoreText.color = Color.yellow;
@@ -83,5 +117,53 @@ public class LogicScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        gameIsPaused = false;
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1.0f;
+        // Re-enable chirping if the user hasn't disabled it
+        chirpAudio.mute = soundEffectsVolume == 0.0f;
+    }
+
+    public void PauseGame()
+    {
+        gameIsPaused = true;
+        // Pause the game
+        Time.timeScale = 0.0f;
+        // Set the text for the pause screen
+        soundEnabledTextString = chirpAudio.mute ? "Sound: Off" : "Sound: On";
+        soundEnabledText.text = soundEnabledTextString;
+        // Show the pause screen
+        pauseScreen.SetActive(true);
+        // Disable chirping
+        chirpAudio.mute = true;
+    }
+
+    [ContextMenu("Chirp")]
+    public void Chirp()
+    {
+        chirpAudio.Play();
+    }
+
+    [ContextMenu("ToggleSounds")]
+    public void ToggleSounds()
+    {
+        if(soundEffectsVolume > 0f)
+        {
+            soundEffectsVolume = 0f;   
+        } else
+        {
+            soundEffectsVolume = 1f;
+        }
+        PlayerPrefs.SetFloat(PREF_SOUND_ENABLED, soundEffectsVolume);
+        checkpointAudio.mute = !checkpointAudio.mute;
+        collisionAudio.mute = !collisionAudio.mute;
+        newHighScoreAudio.mute = !newHighScoreAudio.mute;
+        soundEnabledTextString = collisionAudio.mute ? "Sound: Off" : "Sound: On";
+        soundEnabledText.text = soundEnabledTextString;
+        Debug.Log(string.Format("Sounds enabled? {0}", !checkpointAudio.mute));
     }
 }
